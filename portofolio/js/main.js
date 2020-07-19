a4cbf0d4d82a8d95fe5b48e7ef2d5bdaf8c76960
vidwalk <claudiurediu@gmail.com>
2020-07-19 19:24:17 +0200

    about area and gallery js

---
+++ portofolio/js/main.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+    let $btns = $('.project-area .button-group button');
+
+    $btns.click(function (e) {
+        $('.project-area .button-group button').removeClass('active');
+        e.target.classList.add('active');
+
+        let selector = $(e.target).attr('data-filter');
+        $('.project-area .grid').isotope({
+            filter: selector
+        })
+        return false;
+    })
+
+    $('.project-area .button-group #btn1').trigger('click');
+
+    $('.project-area .grid .test-popup-link').magnificPopup({
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+
+    //owl-carousel init
+    $('.site-main .about-area .owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            560: {
+                items: 2
+            }
+        }
+    })
+
+});
