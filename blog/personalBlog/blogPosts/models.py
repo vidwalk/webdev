f21897176d7229a3815f9af8fe52b009500c69e6
vidwalk <claudiurediu@gmail.com>
2020-08-12 19:18:01 +0200

    Removed Editing and Deleting

--- blog/personalBlog/blogPosts/models.py
+++ blog/personalBlog/blogPosts/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from datetime import datetime,date
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
     title_tag = models.CharField(max_length=255)
+    post_date = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.title 
